ea51a2639270ab056b14ca30f16bf4bbb09dc6e9
Gohel Nayan <gohelnayan264@gmail.com>
2026-01-09 17:39:37 +0530

    deplo

--- Deploy/index.js
+++ Deploy/index.js
@@ -1,1 +1,24 @@
-console.log("hello nayan in backend world");
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/instagram', (req,res) => {
+    res.send('nayangohel31')
+})
+
+app.get('/login', (req,res) => {
+    res.send('<h1>please login first...</h1>')
+})
+
+app.get('/youtube', (req,res) => {
+    res.send('<h2>Priya Nail Art</h2>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
